a6e4f819bf6188d1dc9205a75da217c9a6a27b94
Dunggthee <85491429+Dunggthee@users.noreply.github.com>
2021-08-03 17:48:36 +0700

    upload

---
+++ src/thongtinsv/ThongtinSV.java
@@ -0,0 +1,16 @@
+/*
+ Thong tin sinh vien
+ */
+package thongtinsv;
+
+
+public class ThongtinSV {
+
+    public static void main(String[] args) {
+    System.out.println("Họ Tên : Cao Thế Dũng");
+    System.out.println("Lớp: D08.48.01");
+    System.out.println("Khoa: CNTT");
+    System.out.println("Trường Đại Học Tài Chính Ngân Hàng Hà Nội");
+    }
+    
+}
